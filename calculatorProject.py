18cf180ea6404ee51dbffa76dfcb6221114bd026
Tyge Davis <tyge.davis@gmail.com>
2022-02-04 22:05:48 -0700

    Adding functions for the calculator

--- calculatorProject.py
+++ calculatorProject.py
@@ -1,3 +1,15 @@
+def addTwoNums(num1, num2):
+    return num1 + num2
+
+def subTwoNums(num1, num2):
+    return num1 - num2
+
+def divTwoNums(num1, num2):
+    return num1 / num2
+
+def mulTwoNums(num1, num2):
+    return num1 * num2
+
 print('Welcome to the Python calculator')
 
 num1 = None 
@@ -19,12 +31,12 @@ while type(num2) != int:
 operator = input('What do you want to do? ')
 
 if operator == 'add':
-    print(num1 + num2)
+    print(addTwoNums(num1, num2))
 elif operator == 'sub' or operator == 'subtract':
-    print(num1 - num2)
+    print(subTwoNums(num1, num2))
 elif operator == 'div' or operator == 'divide':
-    print(num1 / num2)
+    print(divTwoNums(num1, num2))
 elif operator == 'mul' or operator == 'multiply':
-    print(num1 * num2)
+    print(mulTwoNums(num1, num2))
 else:
     print('Bye')
